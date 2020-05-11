66fa4fdb7b216922f08f03a6984f6b73c6bc2006
loperasebastian <sebastian.lopera@utp.edu.co>
2020-05-11 11:13:05 -0500

    cambios search,update

--- src/conf/db.js
+++ src/conf/db.js
@@ -2,5 +2,5 @@ module.exports = {
     "host": "localhost",
 	"user": "root",
 	"password": "root",
-	"database": "app_express"
+	"database": "app_server"
 }
--- src/router/router.js
+++ src/router/router.js
@@ -4,6 +4,10 @@ const router = require('express').Router()
 const controller = require('../controller/controller')
 
 router.post('/user',controller.save_user)
+router.delete('/user/:id',controller.delete_user)
+router.get('/user',controller.list_user)
+router.get('/user/:id',controller.search_user)
+router.put('/user/:id',controller.update_user)
 
 
 
