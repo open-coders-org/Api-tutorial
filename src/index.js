62ed6fef65f0e35917aaaabd6ea2c66c71488b06
Edilson <eddylson.londonio@gmail.com>
2020-05-11 19:00:37 -0500

    ls

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ const express = require('express')
 const app = express()
 const {router} = require('./router/router')
 const morgan = require('morgan')
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended : false }))
 app.use('/api',router)
 
-let port = process.env.PORT || 8000
+let port = process.env.PORT || 3000
 
 
 app.listen(port,(err)=> {
